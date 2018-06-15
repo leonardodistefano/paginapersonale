0ad1621e3d64ebe7c9e454343f253ac755286c6c
Leonardo Distefsno <distefanoleonardo.ts@gmail.com>
2018-06-15 22:49:59 +0200

    1

---
+++ Java.js
@@ -0,0 +1,30 @@
+function Opacitychange() {
+  document.getElementById("onda").style.opacity = "1";
+}
+function Opacityback() {
+  document.getElementById("onda").style.opacity = "0";
+}
+function Opacitychange1() {
+  document.getElementById("video").style.opacity = "1";
+}
+function Opacityback1() {
+  document.getElementById("video").style.opacity = "0";
+}
+function Opacitychange2() {
+  document.getElementById("travel").style.opacity = "1";
+}
+
+function Opacityback2() {
+  document.getElementById("travel").style.opacity = "0";
+}
+function Opacitychange3() {
+  document.getElementById("design").style.opacity = "1";
+}
+
+function Opacityback3() {
+  document.getElementById("design").style.opacity = "0";
+}
+
+
+
+
